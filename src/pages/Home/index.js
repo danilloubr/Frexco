dde2b498ec726d4e0682dcd103787143f63ee8f6
Danilo Sousa <danillou@hotmail.com>
2022-01-25 01:24:51 -0300

    CORS

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -16,7 +16,7 @@ function Home() {
     const fetchData = async () => {
       try {
         const { data: resp } = await axios.get(
-          `https://corsanywhere.herokuapp.com/http://www.fruityvice.com/api/fruit/all`,
+          `http://www.fruityvice.com/api/fruit/all`,
           { headers: { "Access-Control-Allow-Origin": "*" } }
         );
 
